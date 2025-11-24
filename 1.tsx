c2d88e7d8398cd556f44e102ab9251f9b3ff6033
Ashish George Aby <ashishgeorgeaby@gmail.com>
2025-11-24 19:28:38 +0530

    Update 1.tsx

--- 1.tsx
+++ 1.tsx
@@ -11,73 +11,91 @@ import { Banner } from './banner/banner.component';
 import { isCancelCase } from '../../utils/cta/cancel-case-cta';
 import { CancelCaseWithPopUpModel } from './cancel-case/cancel-case.component';
 import useAppTrackingHook from './hooks/useAppTracking/Hook';
-import { EMPTY_STRING, NULL, STATUS, UND_STATUS, EMPTY_ARRAY } from './utils/common.utils';
+import {
+  EMPTY_STRING,
+  NULL,
+  STATUS,
+  UND_STATUS,
+  EMPTY_ARRAY,
+  MARGINMD,
+  MARGIN2XL,
+} from './utils/common.utils';
 
 const PageLayout = () => {
   const isMobile = window.innerWidth < 700;
-  const processedPhaseData = useState(EMPTY_ARRAY);
-  
+  const marginMd = MARGINMD;
+  const margin2Xl = MARGIN2XL;
+  const [processedPhaseData, setProcessedPhaseData] = useState(EMPTY_ARRAY);
+
   const { caseTrackingDetails, appTrackingId, isColleagueView } = useAppTrackingHook();
   const trackingdata = caseTrackingDetails?.data;
   const phases = trackingdata?.[0]?.trackingData?.phases as unknown as PhaseType[];
-  
+
   const { isError, updateIsError, commonStaticContent: commonContent } = useApplicationData();
   const { getCaseTrackingResult, isLoading } = useApiRequest();
   const [callApi, setCallApi] = useState(true);
-  
+
   useEffect(() => {
     if (callApi) {
       setCallApi(false);
       getCaseTrackingResult(appTrackingId);
     }
   }, [callApi, updateIsError, isError]);
-  
+
   if (isLoading) {
     return (
-      <Grid.Container>
-        <Grid.Row>
-          <Grid.Col size="1of1">
-            <Loading />
-          </Grid.Col>
-        </Grid.Row>
-      </Grid.Container>
+      <Grid.Col size="1of10">
+        <Loading />
+      </Grid.Col>
     );
   }
-  
+
   if (!appTrackingId) {
     return <ErrorPage />;
   }
-  
+
   return (
     <>
       {isError() ? (
         <ErrorPage />
       ) : (
-        <Page>
-          <Grid.Container>
-            <Grid.Row>
-              <Grid.Col sizeSm="1of1" sizeMd="8of12" sizeLg="8of12">
-                <Header />
+        <Type
+          element="span"
+          data-testid="isMobile"
+          style={
+            isMobile
+              ? { marginTop: margin2Xl, marginLeft: marginMd }
+              : { marginTop: margin2Xl, marginLeft: margin2Xl }
+          }
+        >
+          <Type
+            data-testid="isMobileRight"
+            style={isMobile ? { marginRight: marginMd } : {}}
+            element="span"
+          >
+            <Grid.Col>
+              <Page>
                 {trackingdata?.[0]?.applicationStatus !== EMPTY_STRING &&
                   trackingdata?.[0]?.applicationStatus !== NULL &&
                   trackingdata?.[0]?.applicationStatus !== STATUS && (
-                    <Banner />
+                    <Grid.Col sizeSm="1of1" sizeMd="8of12" sizeLg="8of12">
+                      <Header />
+                      <Banner />
+                    </Grid.Col>
                   )}
                 {trackingdata?.[0]?.applicationStatus?.toLocaleUpperCase() !== UND_STATUS && (
                   <>
-                    {isColleagueView && isCancelCase(phases) && (
-                      <CancelCaseWithPopUpModel />
-                    )}
+                    {isColleagueView && isCancelCase(phases) && <CancelCaseWithPopUpModel />}
                     <PhasesWithStages />
                   </>
                 )}
-              </Grid.Col>
-              <Grid.Col sizeSm="1of1" sizeMd="4of12" sizeLg="4of12">
-                <Sidebar />
-              </Grid.Col>
-            </Grid.Row>
-          </Grid.Container>
-        </Page>
+                <Grid.Col sizeSm="1of1" sizeMd="11of12" sizeLg="4of12">
+                  <Sidebar />
+                </Grid.Col>
+              </Page>
+            </Grid.Col>
+          </Type>
+        </Type>
       )}
     </>
   );
